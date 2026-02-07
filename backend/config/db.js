79af59da17ac3ab755a28b56502c72a69bdf92d1
JeN <jeni8220s@gmail.com>
2026-02-07 15:27:51 +0530

    Add MongoDB connection configuration using mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const dbURI = 'your_mongodb_connection_string'; // Replace with your MongoDB connection string
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB connected successfully.');
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
+
+module.exports = mongoose;
